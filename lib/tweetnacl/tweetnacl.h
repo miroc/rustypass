1e93ba26830622b095c2d1075211bbd515f2e9c9
Miroslav Svitok <mirosvitok@gmail.com>
2015-10-24 10:01:18 +0200

    tweetnacl stream header updated

--- lib/tweetnacl/tweetnacl.h
+++ lib/tweetnacl/tweetnacl.h
@@ -114,34 +114,39 @@ extern int crypto_sign_keypair(unsigned char *,unsigned char *);
 
 // --- crypto_stream ---
 #define crypto_stream_PRIMITIVE "xsalsa20"
-#define crypto_stream crypto_stream_xsalsa20
-#define crypto_stream_xor crypto_stream_xsalsa20_xor
-#define crypto_stream_KEYBYTES crypto_stream_xsalsa20_KEYBYTES
-#define crypto_stream_NONCEBYTES crypto_stream_xsalsa20_NONCEBYTES
-#define crypto_stream_IMPLEMENTATION crypto_stream_xsalsa20_IMPLEMENTATION
-#define crypto_stream_VERSION crypto_stream_xsalsa20_VERSION
-#define crypto_stream_xsalsa20_tweet_KEYBYTES 32
-#define crypto_stream_xsalsa20_tweet_NONCEBYTES 24
-extern int crypto_stream_xsalsa20_tweet(unsigned char *,unsigned long long,const unsigned char *,const unsigned char *);
-extern int crypto_stream_xsalsa20_tweet_xor(unsigned char *,const unsigned char *,unsigned long long,const unsigned char *,const unsigned char *);
-#define crypto_stream_xsalsa20_tweet_VERSION "-"
-#define crypto_stream_xsalsa20 crypto_stream_xsalsa20_tweet
-#define crypto_stream_xsalsa20_xor crypto_stream_xsalsa20_tweet_xor
-#define crypto_stream_xsalsa20_KEYBYTES crypto_stream_xsalsa20_tweet_KEYBYTES
-#define crypto_stream_xsalsa20_NONCEBYTES crypto_stream_xsalsa20_tweet_NONCEBYTES
-#define crypto_stream_xsalsa20_VERSION crypto_stream_xsalsa20_tweet_VERSION
-#define crypto_stream_xsalsa20_IMPLEMENTATION "crypto_stream/xsalsa20/tweet"
-#define crypto_stream_salsa20_tweet_KEYBYTES 32
-#define crypto_stream_salsa20_tweet_NONCEBYTES 8
-extern int crypto_stream_salsa20_tweet(unsigned char *,unsigned long long,const unsigned char *,const unsigned char *);
-extern int crypto_stream_salsa20_tweet_xor(unsigned char *,const unsigned char *,unsigned long long,const unsigned char *,const unsigned char *);
-#define crypto_stream_salsa20_tweet_VERSION "-"
-#define crypto_stream_salsa20 crypto_stream_salsa20_tweet
-#define crypto_stream_salsa20_xor crypto_stream_salsa20_tweet_xor
-#define crypto_stream_salsa20_KEYBYTES crypto_stream_salsa20_tweet_KEYBYTES
-#define crypto_stream_salsa20_NONCEBYTES crypto_stream_salsa20_tweet_NONCEBYTES
-#define crypto_stream_salsa20_VERSION crypto_stream_salsa20_tweet_VERSION
-#define crypto_stream_salsa20_IMPLEMENTATION "crypto_stream/salsa20/tweet"
+#define crypto_stream_KEYBYTES 32
+#define crypto_stream_NONCEBYTES 24
+extern int crypto_stream(unsigned char *,unsigned long long,const unsigned char *,const unsigned char *);
+extern int crypto_stream_xor(unsigned char *,const unsigned char *,unsigned long long,const unsigned char *,const unsigned char *);
+
+// #define crypto_stream crypto_stream_xsalsa20
+// #define crypto_stream_xor crypto_stream_xsalsa20_xor
+// #define crypto_stream_KEYBYTES crypto_stream_xsalsa20_KEYBYTES
+// #define crypto_stream_NONCEBYTES crypto_stream_xsalsa20_NONCEBYTES
+// #define crypto_stream_IMPLEMENTATION crypto_stream_xsalsa20_IMPLEMENTATION
+// #define crypto_stream_VERSION crypto_stream_xsalsa20_VERSION
+// #define crypto_stream_xsalsa20_tweet_KEYBYTES 32
+// #define crypto_stream_xsalsa20_tweet_NONCEBYTES 24
+// extern int crypto_stream_xsalsa20_tweet(unsigned char *,unsigned long long,const unsigned char *,const unsigned char *);
+// extern int crypto_stream_xsalsa20_tweet_xor(unsigned char *,const unsigned char *,unsigned long long,const unsigned char *,const unsigned char *);
+// #define crypto_stream_xsalsa20_tweet_VERSION "-"
+// #define crypto_stream_xsalsa20 crypto_stream_xsalsa20_tweet
+// #define crypto_stream_xsalsa20_xor crypto_stream_xsalsa20_tweet_xor
+// #define crypto_stream_xsalsa20_KEYBYTES crypto_stream_xsalsa20_tweet_KEYBYTES
+// #define crypto_stream_xsalsa20_NONCEBYTES crypto_stream_xsalsa20_tweet_NONCEBYTES
+// #define crypto_stream_xsalsa20_VERSION crypto_stream_xsalsa20_tweet_VERSION
+// #define crypto_stream_xsalsa20_IMPLEMENTATION "crypto_stream/xsalsa20/tweet"
+// #define crypto_stream_salsa20_tweet_KEYBYTES 32
+// #define crypto_stream_salsa20_tweet_NONCEBYTES 8
+// extern int crypto_stream_salsa20_tweet(unsigned char *,unsigned long long,const unsigned char *,const unsigned char *);
+// extern int crypto_stream_salsa20_tweet_xor(unsigned char *,const unsigned char *,unsigned long long,const unsigned char *,const unsigned char *);
+// #define crypto_stream_salsa20_tweet_VERSION "-"
+// #define crypto_stream_salsa20 crypto_stream_salsa20_tweet
+// #define crypto_stream_salsa20_xor crypto_stream_salsa20_tweet_xor
+// #define crypto_stream_salsa20_KEYBYTES crypto_stream_salsa20_tweet_KEYBYTES
+// #define crypto_stream_salsa20_NONCEBYTES crypto_stream_salsa20_tweet_NONCEBYTES
+// #define crypto_stream_salsa20_VERSION crypto_stream_salsa20_tweet_VERSION
+// #define crypto_stream_salsa20_IMPLEMENTATION "crypto_stream/salsa20/tweet"
 
 // --- crypto_verify ---
 #define crypto_verify_PRIMITIVE "16"
